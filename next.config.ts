c28261a0f8c4ec8a85143e8175f71e2feb7fc646
Andrej Srna <srna@czs.muni.cz>
2026-03-22 14:22:23 +0100

    Allow NJK CDN images in Next.js

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,7 @@ const nextConfig: NextConfig = {
       { protocol: 'https', hostname: 'localhost', port: '1337', pathname: '**' },
       { protocol: 'https', hostname: 'cdn.nocopyrightgamingmusic.com', pathname: '**' },
       { protocol: 'https', hostname: 'cdn.fitdoplnky.sk', pathname: '**' },
+      { protocol: 'https', hostname: 'cdn.najsilnejsiaklbovavyziva.sk', pathname: '**' },
       { protocol: 'https', hostname: 'i.ytimg.com', pathname: '**' },
       { protocol: 'https', hostname: 'img.youtube.com', pathname: '**' },
       { protocol: 'https', hostname: 's3.amazonaws.com', pathname: '**' },
